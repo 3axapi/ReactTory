2d8528c6064359c2796faa3f43672356ebd942de
js <js@cos.pl>
2023-01-12 15:23:56 +0100

    second 12.01.2023

--- counter-app/src/components/movies.js
+++ counter-app/src/components/movies.js
@@ -6,15 +6,6 @@ class Movies extends Component {
     state = {
         movies: getMovies()
     }
-
-    /* <p> Show movies from {this.state.movies[1].title} </p> */
-
-    /*
-        zrobić tabelę, która wyświetli wszystkie filmy
-        tabela w html
-        kolumny to id, title, genre, ... z listy filmów
-        this.state.movies.map( movie => ... )
-    */
     
     render() {
         return (
@@ -23,7 +14,7 @@ class Movies extends Component {
                     <tr>
                         <th>Id</th>
                         <th>Title</th>
-                        <th>Genre</th>
+                        <th colSpan={2}>Genre</th>
                         <th>NumberInStock</th>
                         <th>DailyRentalRate</th>
                         <th>PublishDate</th>
@@ -35,6 +26,7 @@ class Movies extends Component {
                                     <tr>
                                         <td>{element._id}</td>
                                         <td>{element.title}</td>
+                                        <td>{element.genre._id}</td>
                                         <td>{element.genre.name}</td>
                                         <td>{element.numberInStock}</td>
                                         <td>{element.dailyRentalRate}</td>
