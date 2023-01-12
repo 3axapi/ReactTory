bcc51f48fbb754342bed8217b37608f1cffdcc13
js <js@cos.pl>
2023-01-12 15:06:19 +0100

    12.01.2023

--- counter-app/src/components/movies.js
+++ counter-app/src/components/movies.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, StyleSheet } from 'react';
 import { getMovies } from './fake_movies';
 
 class Movies extends Component {
@@ -7,19 +7,18 @@ class Movies extends Component {
         movies: getMovies()
     }
 
-    
+    /* <p> Show movies from {this.state.movies[1].title} </p> */
+
+    /*
+        zrobić tabelę, która wyświetli wszystkie filmy
+        tabela w html
+        kolumny to id, title, genre, ... z listy filmów
+        this.state.movies.map( movie => ... )
+    */
     
     render() {
-
         return (
-            <>
-                <p> Show movies from {this.state.movies[0].title} </p>
-                {/*
-                    zrobić tabelę, która wyświetli wszystkie filmy
-                    tabela w html
-                    kolumny to id, title, genre, ... z listy filmów
-                    this.state.movies.map( movie => ... )
-                */}
+            <>        
                 <table>
                     <tr>
                         <th>Id</th>
@@ -29,6 +28,22 @@ class Movies extends Component {
                         <th>DailyRentalRate</th>
                         <th>PublishDate</th>
                     </tr>
+                    {   
+                        this.state.movies.map(
+                            element => {
+                                return (
+                                    <tr>
+                                        <td>{element._id}</td>
+                                        <td>{element.title}</td>
+                                        <td>{element.genre.name}</td>
+                                        <td>{element.numberInStock}</td>
+                                        <td>{element.dailyRentalRate}</td>
+                                        <td>{element.publishDate}</td>
+                                    </tr>
+                                )
+                            }
+                        )
+                    }
                 </table>
             </>
         )
